8af4c61c602f3ab5f3533853dc82c35aab9311be
Martin Ellegård <martin.ellegard@gmail.com>
2023-02-06 01:55:35 +0100

    Base rust project added

---
+++ rust-api/src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
