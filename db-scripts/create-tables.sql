07474fb31d1d3ddcbfe08954ae4f21cd41da81eb
Martin Ellegård <martin.ellegard@gmail.com>
2023-02-10 11:04:23 +0100

    Db update added base rust func for managing users

--- db-scripts/create-tables.sql
+++ db-scripts/create-tables.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS "skills_correlation";
 DROP TABLE IF EXISTS "skills_users";
+DROP TABLE IF EXISTS "project_skills";
 DROP TABLE IF EXISTS "skills";
 DROP TABLE IF EXISTS "project_users";
 DROP TABLE IF EXISTS "projects";
+DROP TABLE IF EXISTS "oauth_access_tokens";
 DROP TABLE IF EXISTS "users";
 DROP TABLE IF EXISTS "companies";
 
@@ -10,8 +12,8 @@ DROP TABLE IF EXISTS "companies";
 CREATE TABLE IF NOT EXISTS "companies" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
     "name" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
 
@@ -23,8 +25,8 @@ CREATE TABLE IF NOT EXISTS "users" (
     "description" varchar(255) NOT NULL,
     "email" varchar(255) NOT NULL,
     "company_id" INT,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_company
       FOREIGN KEY(company_id) 
@@ -37,7 +39,7 @@ CREATE TABLE IF NOT EXISTS "oauth_access_tokens" (
     "id" INT GENERATED always AS IDENTITY,
     "user_id" INT NOT NULL,
     "access_token" varchar(255) NOT NULL,
-    "expires_at" timestamp NOT NULL,
+    "expires_at" timestamp with time zone NOT NULL,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
@@ -50,8 +52,8 @@ CREATE TABLE IF NOT EXISTS "projects" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
     "name" varchar(255) NOT NULL,
     "description" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
 
@@ -60,18 +62,18 @@ CREATE TABLE IF NOT EXISTS "skills" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
     "name" varchar(255) NOT NULL,
     "use" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
 
 --Create project skills table
 CREATE TABLE IF NOT EXISTS "project_skills" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
-    "project_id" INTEGER NOT NULL,
-    "skill_id" INTEGER NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "project_id" INT NOT NULL,
+    "skill_id" INT NOT NULL,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_project
       FOREIGN KEY(project_id) 
@@ -84,10 +86,10 @@ CREATE TABLE IF NOT EXISTS "project_skills" (
 --Create Project Users table
 CREATE TABLE IF NOT EXISTS "project_users" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
-    "project_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "project_id" INT NOT NULL,
+    "user_id" INT NOT NULL,
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_project
       FOREIGN KEY(project_id) 
@@ -100,11 +102,11 @@ CREATE TABLE IF NOT EXISTS "project_users" (
 --Create skills users table
 CREATE TABLE IF NOT EXISTS "skills_users" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
-    "skill_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
-    "proficiency" INTEGER NOT NULL, -- 1-5
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "skill_id" INT NOT NULL,
+    "user_id" INT NOT NULL,
+    "proficiency" INT NOT NULL, -- 1-5
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     Constraint fk_skill
       FOREIGN KEY(skill_id)
@@ -117,11 +119,11 @@ CREATE TABLE IF NOT EXISTS "skills_users" (
 --Create skill corelation table
 CREATE TABLE IF NOT EXISTS "skills_correlation" (
     "id" INT GENERATED ALWAYS AS IDENTITY,
-    "skill_primary" INTEGER NOT NULL,
-    "skill_secondary" INTEGER NOT NULL,
-    "correlation" INTEGER NOT NULL, -- 1-10
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "skill_primary" INT NOT NULL,
+    "skill_secondary" INT NOT NULL,
+    "correlation" INT NOT NULL, -- 1-10
+    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_skill_primary
       FOREIGN KEY(skill_primary)
--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,9 +1,9 @@
-use core::time;
+use std::env;
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use time::OffsetDateTime;
 use serde::{Serialize, Deserialize};
 use sqlx::postgres::{PgPoolOptions, PgPool};
-use chrono::{NaiveDateTime, Utc};
 
 //Actix web state
 #[derive(Clone)]
@@ -11,53 +11,120 @@ struct AppState {
     pool: PgPool
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello world!"
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let db_env_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_url = db_env_url.as_str();
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:PassW0rd@localhost:5432/postgres")
+        .connect(db_url)
         .await
         .unwrap();
 
     let app_state = AppState {
         pool: pool,
     };
-    
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(app_state.clone()))
-            .service(index)
+            .route("/", web::get().to(index))
+            .route("/users", web::get().to(get_users))
+            .route("/users/{id}", web::get().to(get_user))
+            .route("/users", web::post().to(post_user))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-#[derive(Serialize, Deserialize)]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct User {
     id: i32,
     firstname: String,
     lastname: String,
     description: String,
     email: String,
-    company_id: i32,
-    created_at: time,
-    updated_at: time
+    company_id: Option<i32>,
+    created_at: OffsetDateTime,
+    updated_at: OffsetDateTime,
 }
 
-#[get("/users")]
-async fn users(app_state: web::Data<AppState>) -> impl Responder {
-    let users = sqlx::query!(
-        Vec<User>,
+async fn get_users(app_state: web::Data<AppState>) -> HttpResponse {
+    let users = sqlx::query_as!(
+        User,
         "SELECT * FROM users")
         .fetch_all(&app_state.pool)
-        .await
-        .unwrap();
+        .await;
 
-    format!("{:?}", users)
+    match users {
+        Ok(users) => {
+            HttpResponse::Ok().json(users)
+        },
+        Err(e) => {
+            HttpResponse::Ok().body(e.to_string())
+        }
+    }
+}
+
+async fn get_user(app_state: web::Data<AppState>, path: web::Path<i32>) -> HttpResponse {
+    let id = path.into_inner();
+    let user = sqlx::query_as!(
+        User,
+        "SELECT * FROM users WHERE id = $1",
+        id)
+        .fetch_one(&app_state.pool)
+        .await;
+
+    match user {
+        Ok(user) => {
+            HttpResponse::Ok().json(user)
+        },
+        Err(e) => {
+            HttpResponse::Ok().body(e.to_string())
+        }
+    }
+}
+
+async fn post_user(app_state: web::Data<AppState>, user: web::Json<User>) -> HttpResponse {
+    let user_exist = sqlx::query!(
+        "SELECT * FROM users WHERE email = $1",
+        user.email)
+        .fetch_one(&app_state.pool)
+        .await;
+    
+    match user_exist {
+        Ok(_) => {
+            return HttpResponse::Conflict().body("User already exist");
+        },
+        Err(_) => {}
+    }
+
+    let user = sqlx::query_as!(
+        User,
+        "INSERT INTO users (firstname, lastname, description, email, company_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        user.firstname,
+        user.lastname,
+        user.description,
+        user.email,
+        user.company_id,
+        user.created_at,
+        user.updated_at)
+        .fetch_one(&app_state.pool)
+        .await;
+
+    match user {
+        Ok(user) => {
+            HttpResponse::Ok().json(user)
+        },
+        Err(e) => {
+            HttpResponse::Ok().body(e.to_string())
+        }
+    }
 }
