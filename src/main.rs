749fca68c2bc36b77928641b4d35f55b7916f94d
Martin Ellegård <martin.ellegard@gmail.com>
2023-03-23 20:33:28 +0100

    fixed issue with wrong port number

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,17 @@ async fn main() -> std::io::Result<()> {
     //Initialize logger
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     dotenv::dotenv().ok();
+
+    // Read the port number from the database connection string from env
     let db_env_url =
         env::var("DATABASE_CONNECTION").expect("DATABASE_URL must be set in .env file");
+
+    // Read the port number from the PORT environment variable or use a default value
+    let port: u16 = env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080);
+
     let repository = repository::Repository::new(db_env_url).await;
 
     let app_state = AppState { repository };
@@ -33,7 +42,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/api").configure(api::scoped_config))
             .route("/", web::get().to(index))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
