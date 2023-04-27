e1164506c026c2f14aed6cd338f47d5a4378d387
Martin Ellegård <martin.ellegard@gmail.com>
2023-04-27 22:54:14 +0200

    Added auth to profile methods

--- src/api.rs
+++ src/api.rs
@@ -1,44 +1,41 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-use actix_web::{web, Responder, HttpResponse};
+use actix_web::{web, HttpResponse, Responder};
 use serde_json::json;
+use std::time::{SystemTime, UNIX_EPOCH};
 use uuid::Uuid;
 
-use crate::{AppState, schemas, auth::Claims};
+use crate::{
+    auth::{Claims, Jwt},
+    schemas, AppState,
+};
 //use actix_web_httpauth::extractors::bearer::BearerAuth;
-
 
 pub fn scoped_config(cfg: &mut web::ServiceConfig) {
     //User routes
-    cfg
-    .service(
-        web::resource("/register")
-            .route(web::post().to(register_user)),
-    )
-    .service(
-        web::resource("/profiles")
-            .route(web::get().to(get_profiles))
-            .route(web::post().to(post_profile))
-    )
-    .service(
-        web::resource("/profiles/{id}")
-            .route(web::get().to(get_profile_by_id))
-            //.route(web::put().to(profiles::put_user))
-    )
-    .service(
-        web::resource("/profiles/skills/{skills_string}")
-            .route(web::get().to(get_profiles_by_skills)),
-    )
-    .service(
-        web::resource("/auth")
-            .route(web::post().to(authenticate_user)),
-    );
+    cfg.service(web::resource("/register").route(web::post().to(register_user)))
+        .service(
+            web::resource("/profiles")
+                .route(web::get().to(get_profiles))
+                .route(web::post().to(post_profile)),
+        )
+        .service(
+            web::resource("/profiles/id/{id}").route(web::get().to(get_profile_by_id)), //.route(web::put().to(profiles::put_user))
+        )
+        .service(web::resource("/profiles/me").route(web::get().to(get_profile_by_user)))
+        .service(
+            web::resource("/profiles/skills/{skills_string}")
+                .route(web::get().to(get_profiles_by_skills)),
+        )
+        .service(web::resource("/auth").route(web::post().to(authenticate_user)));
     // cfg.service(
     //     web::resource("/profiles/{id}/skills")
     //         .route(web::get().to(profiles::get_user_skills))
     // );
 }
 
-pub async fn register_user(data: web::Data<AppState>, new_user_no_id_json: web::Json::<schemas::UserNoId>) -> impl Responder {
+pub async fn register_user(
+    data: web::Data<AppState>,
+    new_user_no_id_json: web::Json<schemas::UserNoId>,
+) -> impl Responder {
     let new_user_no_id = new_user_no_id_json.into_inner();
     let new_user = schemas::User {
         id: Uuid::new_v4().to_string(),
@@ -55,17 +52,25 @@ pub async fn register_user(data: web::Data<AppState>, new_user_no_id_json: web::
     }
 }
 
-pub async fn authenticate_user(data: web::Data<AppState>, user: web::Json::<schemas::UserNoId>) -> impl Responder {
-    let result = data.repository.authenticate_user(&user.username, &user.password).await;
+pub async fn authenticate_user(
+    data: web::Data<AppState>,
+    user: web::Json<schemas::UserNoId>,
+) -> impl Responder {
+    let result = data
+        .repository
+        .authenticate_user(&user.username, &user.password)
+        .await;
     match result {
         Ok(Some(user)) => {
             let secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
-            let expiration = (SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as usize) + 3600; // 1 hour
+            let expiration = (SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as usize)
+                + 3600; // 1 hour
             let claims = Claims::new(user.id, expiration);
             match claims.encode(secret.as_bytes()) {
-                Ok(token) => {
-                    HttpResponse::Ok().json(json!({ "token": token }))
-                },
+                Ok(token) => HttpResponse::Ok().json(json!({ "token": token })),
                 Err(e) => {
                     eprintln!("Error encoding token: {}", e);
                     HttpResponse::InternalServerError().finish()
@@ -111,22 +116,23 @@ pub async fn authenticate_user(data: web::Data<AppState>, user: web::Json::<sche
 //     }
 // }
 
-pub async fn get_profiles(app_state: web::Data<AppState>) -> HttpResponse {
+pub async fn get_profiles(app_state: web::Data<AppState>, _: Jwt) -> HttpResponse {
     let profiles = app_state.repository.get_all_profiles().await;
     match profiles {
-        Ok(profiles) => {
-            HttpResponse::Ok().json(profiles)
-        },
-        Err(e) => {
-            HttpResponse::InternalServerError().body(e.to_string())
-        }
+        Ok(profiles) => HttpResponse::Ok().json(profiles),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
 
-pub async fn post_profile(app_state: web::Data<AppState>, profile_json: web::Json<schemas::ProfileNoId>) -> HttpResponse {
+pub async fn post_profile(
+    app_state: web::Data<AppState>,
+    profile_json: web::Json<schemas::ProfileNoId>,
+    jwt: Jwt,
+) -> HttpResponse {
+    let id = Uuid::parse_str(&jwt.0.as_str()).unwrap_or(Uuid::new_v4());
     let profile_no_id = profile_json.into_inner();
-    let profile = schemas::Profile{
-        id: Uuid::new_v4(),
+    let profile = schemas::Profile {
+        id,
         name: profile_no_id.name,
         email: profile_no_id.email,
         description: profile_no_id.description,
@@ -140,35 +146,51 @@ pub async fn post_profile(app_state: web::Data<AppState>, profile_json: web::Jso
     };
     let result = app_state.repository.create_profile(profile).await;
     match result {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
-            HttpResponse::InternalServerError().body(e.to_string())
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
 
-pub async fn get_profile_by_id(app_state: web::Data<AppState>, profile_id: web::Path<Uuid>) -> HttpResponse {
-    let profile = app_state.repository.get_profiles_by_id(profile_id.into_inner()).await;
+pub async fn get_profile_by_id(
+    app_state: web::Data<AppState>,
+    profile_id: web::Path<Uuid>,
+    _: Jwt,
+) -> HttpResponse {
+    let profile = app_state
+        .repository
+        .get_profiles_by_id(profile_id.into_inner())
+        .await;
     match profile {
-        Ok(profile) => {
-            HttpResponse::Ok().json(profile)
-        },
-        Err(e) => {
-            HttpResponse::InternalServerError().body(e.to_string())
-        }
+        Ok(profile) => HttpResponse::Ok().json(profile),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
 
-pub async fn get_profiles_by_skills(app_state: web::Data<AppState>, skills_string: web::Path<String>) -> HttpResponse {
-    let profile = app_state.repository.get_profiles_by_skills(skills_string.into_inner()).await;
-    match profile {
-        Ok(profile) => {
-            HttpResponse::Ok().json(profile)
-        },
-        Err(e) => {
-            HttpResponse::InternalServerError().body(e.to_string())
+pub async fn get_profile_by_user(app_state: web::Data<AppState>, jwt: Jwt) -> HttpResponse {
+    let id = Uuid::parse_str(&jwt.0.as_str());
+    match id {
+        Ok(id) => {
+            let profile = app_state.repository.get_profiles_by_id(id).await;
+            match profile {
+                Ok(profile) => HttpResponse::Ok().json(profile),
+                Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+            }
         }
+        Err(e) => HttpResponse::BadRequest().body(e.to_string()),
+    }
+}
+
+pub async fn get_profiles_by_skills(
+    app_state: web::Data<AppState>,
+    skills_string: web::Path<String>,
+    _: Jwt,
+) -> HttpResponse {
+    let profile = app_state
+        .repository
+        .get_profiles_by_skills(skills_string.into_inner())
+        .await;
+    match profile {
+        Ok(profile) => HttpResponse::Ok().json(profile),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
